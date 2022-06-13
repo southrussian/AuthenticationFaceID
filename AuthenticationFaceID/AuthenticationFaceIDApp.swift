4f6a4b1c6c4409b6ea357ddb0ca2583e1d3f5086
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-14 00:52:10 +0300

    Initial Commit

---
+++ AuthenticationFaceID/AuthenticationFaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationFaceIDApp.swift
+//  AuthenticationFaceID
+//
+//  Created by Danil Peregorodiev on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationFaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
